4b483edbcde458e09e72d454724419c35f0eba54
Richard Skumat <gitlab@richardskumat.com>
2019-07-29 01:50:44 +0100

    add a script that adds back remotes on my desktop

---
+++ add_remotes_back.sh
@@ -0,0 +1,3 @@
+git remote add github git@github:richardskumat/ansible-role-grub_cmdline
+git remote add gitlab git@gitlab:richardskumat/ansible-role-grub_cmdline
+git remote add origin git@wsgitlab:qwe/ansible-role-grub_cmdline
